f0cc976b6657c94a10e3eaedfc8c7b5de2ce61e5
Jacoboo <jacobo.diaz-a@mail.escuelaing.edu.co>
2025-09-15 19:56:32 -0500

    Jacobo: corrección mvn

---
+++ SIRHA-BackEnd-DOSW/src/main/java/edu/dosw/sirah/SirhaBackEndDoswApplication.java
@@ -0,0 +1,14 @@
+package edu.dosw.sirah;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirhaBackEndDoswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirhaBackEndDoswApplication.class, args);
+	}
+
+}
---
+++ SIRHA-BackEnd-DOSW/src/test/java/edu/dosw/sirah/SIRHA_BackEnd_DOSW/SirhaBackEndDoswApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.sirah.SIRHA_BackEnd_DOSW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirhaBackEndDoswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
